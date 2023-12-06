4be981a5a244f2ad186ac80a28ce43953f8e5e33
Aleksandar Cambas <aleksandarcambas@gmail.com>
2023-12-06 21:51:20 +0100

    luasnip fix

--- .config/nvim/lua/acambas/plugins/lsp-zero.lua
+++ .config/nvim/lua/acambas/plugins/lsp-zero.lua
@@ -20,7 +20,13 @@ return {
 			"hrsh7th/nvim-cmp",
 			"nvim-tree/nvim-web-devicons",
 			"onsails/lspkind.nvim",
-			"L3MON4D3/LuaSnip",
+			{
+				"L3MON4D3/LuaSnip",
+				-- follow latest release.
+				version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+				-- install jsregexp (optional!).
+				-- build = "make install_jsregexp",
+			},
 			"saadparwaiz1/cmp_luasnip",
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-path",
